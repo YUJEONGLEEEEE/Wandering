8e179ad71168140a4ce3991cc8cdfa95b5c7dc24
유정 <llyujay95@gmail.com>
2025-06-16 22:26:48 +0900

    chore: integrate Firebase and GoogleSignIn packages

---
+++ Wandering/Wandering/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Wandering
+//
+//  Created by 이유정 on 6/16/25.
+//
+
+import SwiftUI
+import FirebaseCore
+import GoogleSignIn
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey:  Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
