fd75963ec1207bdc19d9596b98b3790223f77e51
유정 <llyujay95@gmail.com>
2025-06-25 15:02:26 +0900

    chore: remove Google Sign-In feature, API key, and related code from all files for security reasons

--- Wandering/Wandering/Views/OnboardingView.swift
+++ Wandering/Wandering/Views/OnboardingView.swift
@@ -46,6 +46,8 @@ struct OnboardingView: View {
         }
     }
 //    #Preview {
-//        OnboardingView()
+//        NavigationStack{
+//            OnboardingView()
+//        }
 //    }
 }
--- Wandering/Wandering/WanderingApp.swift
+++ Wandering/Wandering/WanderingApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
 
 @main
 struct WanderingApp: App {
+
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             OnboardingView()
